0bb9dd251fff488b9845fd6cf21103d9243a16f9
yohesh1197 <55386499+yohesh1197@users.noreply.github.com>
2023-10-18 12:45:25 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 echo "inside-sh file"
 
 # Directory where your Node.js application is deployed
-APP_DIR="/home/ubuntu/node-connect-server-dev-test/"
+APP_DIR="/home/ubuntu/node-connect-server-dev-test/node-connect-server/"
 
 # Remote Git repository URL
 GIT_REPO="git@github.com:iomdi2i/node-connect-server.git"
@@ -13,16 +13,17 @@ cd $APP_DIR
 ls
 
 # Pull the latest code from your Git repository
-git clone $GIT_REPO
-cd node-connect-server/
-git pull origin dev
+# git clone $GIT_REPO
+# cd node-connect-server/
+git pull upstream dev
 
 # Install or update Node.js application dependencies (assumes you're using npm)
-npm install
+npm run install_deps
 
+npm run dev
 # Restart your Node.js application
 # Replace 'app.js' with the main entry point of your Node.js application
-pm2 restart app.js  # You can use pm2 or another process manager of your choice
+# pm2 restart app.js  # You can use pm2 or another process manager of your choice
 
 # Exit the SSH session
 exit 0
